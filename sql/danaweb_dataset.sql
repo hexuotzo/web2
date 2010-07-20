02ff4c16bd0efeba49ad5edf01a8a654e42e1c5e
hexuotzo <hexuotzo@hexuotzo-laptop.(none)>
2010-07-20 15:11:47 +0800

    增加sql文件

---
+++ sql/danaweb_dataset.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog 企业版 - MySQL GUI v7.14 
+MySQL - 5.0.67-log : Database - log_analysis2
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+USE `log_analysis2`;
+
+/*Data for the table `danaweb_dataset` */
+
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (1,'call_city_day','KPI查询次数指标日数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (2,'call_city_week','KPI查询次数指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (3,'call_city_month','KPI查询次数指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (4,'user_city_day','KPI用户数指标日数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (5,'user_city_week','KPI用户数指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (6,'user_city_month','KPI用户数指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (7,'user_search_city_day','KPI信息查询用户数指标日数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (8,'user_search_city_week','KPI信息查询用户数指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (9,'user_search_city_month','KPI信息查询用户数指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (10,'user_travel_city_day','KPI商旅用户数指标日数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (11,'user_travel_city_week','KPI商旅用户数指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (12,'user_travel_city_month','KPI商旅用户数指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (13,'his_call_city_week','KPI累计查询次数指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (14,'his_call_city_month','KPI累计查询次数指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (15,'his_user_city_week','KPI累计用户数指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (16,'his_user_city_month','KPI累计用户数指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (17,'his_user_search_city_week','KPI累计信息查询用户数指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (18,'his_user_search_city_month','KPI累计信息查询用户数指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (19,'his_user_travel_city_week','KPI累计商旅用户数指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (20,'his_user_travel_city_month','KPI累计商旅用户数指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (21,'sales_city_day','KPI语音后向销售指标日数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (22,'sales_city_week','KPI语音后向销售指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (23,'sales_city_month','KPI语音后向销售指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (24,'travel_account_city_day','KPI商旅业务量指标日数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (25,'travel_account_city_week','KPI商旅业务量指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (26,'travel_account_city_month','KPI商旅业务量指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (27,'shbb_payusernum_city_day','KPI生活播报指标日数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (28,'shbb_payusernum_city_week','KPI生活播报指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (29,'shbb_payusernum_city_month','KPI生活播报指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (30,'his_travel_account_city_week','KPI累计商旅业务量周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (31,'his_travel_account_city_month','KPI累计商旅业务量月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (40,'shbb_total_payusers_province_day','KPI生活播报指标(累计)日数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (33,'wireless_city_week','KPI前向业务指标周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (34,'wireless_city_month','KPI前向业务指标月数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (38,'buss_wireless_city_week','KPI前向业务指标(分业务)周数据','');
+insert  into `danaweb_dataset`(`id`,`name`,`cname`,`sql_sentence`) values (39,'buss_wireless_city_month','KPI前向业务指标(分业务)月数据','');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
